e8de942c07e5a9a32487ce967069333358458c16
trolleway <trolleway@yandex.ru>
2016-11-16 12:53:42 +0300

    create folder ngw2ngw

---
+++ ngw2ngw/ngw2ngw.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Project: Two-directional synchrosnisation of 2 ngw layers
+# Author: Artem Svetlov <artem.svetlov@nextgis.com>
+# Copyright: 2016, NextGIS <info@nextgis.com>
+
+
+
+'''
+
+'''
+
+
