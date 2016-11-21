7a1a90a45feb065441a79e8ecc5b39ceae8ec01d
Artem Svetlov <trolleway@yandex.ru>
2016-11-21 15:19:04 +0300

    Update config.example.py

--- ngw2ngw/config.example.py
+++ ngw2ngw/config.example.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
-#cedar api
-dict_url = 'http://'
-dict_creds = ('', '')
+#ngw api
+ngw1_url = ''
+ngw1_resource_id = ''
+ngw1_creds = ('', '')
+ngw1_dump_today_filename = 'dump_ngw1_now.json'
+ngw1_dump_yesterday_filename = 'dump_ngw1_prev.json'
 
 #ngw api
-ngw_url = 'http://t.nextgis.com/ngw/api/resource/'
-ngw_creds = ('', '')
-ngw_resourse_id = '766'
+ngw2_url = ''
+ngw2_resource_id = ''
+ngw2_creds = ('', '')
+ngw2_dump_today_filename = 'dump_ngw2_now.json'
+ngw2_dump_yesterday_filename = 'dump_ngw2_prev.json'
+
+
 
 timeout = 3
