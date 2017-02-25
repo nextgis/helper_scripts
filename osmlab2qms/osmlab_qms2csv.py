f277b9c24dff10f40dc084de69d45d0c7bc5de3d
Maxim Dubinin <sim@gis-lab.info>
2017-02-25 14:07:17 +0300

    many improvements

--- osmlab2qms/osmlab_qms2csv.py
+++ osmlab2qms/osmlab_qms2csv.py
@@ -1,23 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Project: Валидатор наличия в QMS слоёв из набора OSMLAB
-# Author: Artem Svetlov <artem.svetlov@nextgis.com>
-# Copyright: 2016, NextGIS <info@nextgis.com>
+#******************************************************************************
+#
+# osmlab_qms2csv.py
+# ---------------------------------------------------------
+# Send notifications about new QMS services to Telegram channel through a bot
+# More: http://github.com/nextgis/helper_scripts/osmlab
+#
+# Usage: 
+#      usage: osmlab_qms2csv.py [-h] [-u]
+#      where:
+#           -h              show this help message and exit
+# Example:
+#      python osmlab_qms2csv.py -u
+#
+# Copyright (C) 2017-present Maxim Dubinin (maxim.dubinin@nextgis.com), Artem Svetlov <artem.svetlov@nextgis.com>
+#
+# This source is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# A copy of the GNU General Public License is available on the World Wide Web
+# at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+# to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+#******************************************************************************
 
-import urllib
+import requests
+import argparse
 import json
 from pprint import pprint
 import os
 import urlparse
 import re
 import csv
+import countryinfo
 
-def downloadqms():
+parser = argparse.ArgumentParser()
+parser.add_argument('-u','--update', action="store_true", help='Update data')
+args = parser.parse_args()
+
+def downloadOSMLab(filename):
+    #read file from github
+    url='https://github.com/osmlab/editor-layer-index/raw/gh-pages/imagery.json'
+
+    r = requests.get(url)
+    with open(filename, "wb") as data_file:
+        data_file.write(r.content)
+    print('Fresh OSMLab imagery.json dowloaded')
+    with open(filename) as data_file:    
+        data = json.load(data_file)
+
+    return data
+
+def downloadQMS(filename):
     #qmslist=import_qms()
     url='https://qms.nextgis.com/api/v1/geoservices/?format=json'
-    filename='qms.json'
-    testfile = urllib.URLopener()
-    testfile.retrieve(url, filename)
+    r = requests.get(url)
+    
+    with open(filename, "wb") as data_file:
+        data_file.write(r.content)
+
     with open(filename) as data_file:    
         qmslist = json.load(data_file)
 
@@ -27,8 +77,8 @@ def downloadqms():
 
         url='https://qms.nextgis.com/api/v1/geoservices/'+str(layer['id'])+'/?format=json'
         print url
-        response = urllib.urlopen(url)
-        data = json.loads(response.read())
+        r = requests.get(url)
+        data = json.loads(r.content)
         layer.update(data)
 
         #print layer
@@ -36,8 +86,8 @@ def downloadqms():
         with open('qms_full.json', 'wb') as outfile:
             json.dump(newlist, outfile)
 
-def openqmsfile():
-    with open('qms_full.json') as data_file:
+def openjson(filename):
+    with open(filename) as data_file:
         data = json.load(data_file)
 
     return data
@@ -61,34 +111,74 @@ def getLayerDomain(url):
 
 def prepare_url(url):
     url = url.replace('//a.','//')
-    url = url.replace('http://','',1)
-    url = url.replace('https://','',1)
+    if url.find('http://') == 0: url = url.replace('http://','',1)
+    if url.find('https://') == 0: url = url.replace('https://','',1)
     url = url.rstrip('?')
     url = url.upper()
 
     return url
 
+def find_noimport(service_id):
+    noimport = open('noimport.txt').read().splitlines()
+    if service_id in noimport: 
+        result = True
+    else:
+        result = False
+
+    return result
+
 def find_qmsTMS(url):
     #   Search in qms for layer
-    qmslist=openqmsfile()
     exist_qms = False
+    layer = None
     for qmslayer in qmslist:
         if prepare_url(url) in qmslayer['url'].upper():
                 exist_qms = True
+                layer = qmslayer
 
-    return exist_qms
+    return layer, exist_qms
 
 def find_qmsWMS(url,layers):
     #   Search in qms for layer
-    qmslist=openqmsfile()
     exist_qms = False
+    layer = None
     for qmslayer in qmslist:
         if prepare_url(url) in qmslayer['url'].upper() and qmslayer['type'] == 'wms':
             if qmslayer['layers'].upper() == layers.upper():
                 exist_qms = True
+                layer = qmslayer
 
-    return exist_qms
+    return layer, exist_qms
 
+def find_changes(qmslayer,layer):
+    find_changes = ''
+
+    if len(qmslayer['desc'].split(':')) > 1 and layer['id'] == qmslayer['desc'].split(':')[1].split('. ')[0].strip():
+        #this service is synced with OSMLab
+        if 'license_url' in layer.keys() and layer['license_url'] != qmslayer['license_url']:
+            find_changes = find_changes + ';' + 'license_url'
+        if len(qmslayer['desc'].split(':')) >2:
+            cntry = [x for x in countryinfo.countries if x['name'] == qmslayer['desc'].split(':')[2].strip()]
+            if layer['country_code'] != cntry[0]['code']:
+                find_changes = find_changes + ';' + 'country'
+        if layer['type'] != qmslayer['type']:
+            find_changes = find_changes + ';' + 'type'    
+        if 'attribution' in layer.keys():
+            if 'text' in layer['attribution'].keys() and layer['attribution']['text'].strip() != qmslayer['copyright_text']:
+                find_changes = find_changes + ';' + 'copyright_text'
+            if 'url' in layer['attribution'].keys() and layer['attribution']['url'] != qmslayer['copyright_url']:
+                find_changes = find_changes + ';' + 'copyright_url'
+        if layer['name'] != qmslayer['name'] and layer['name'] + ' WMS' != qmslayer['name'] and layer['name'] + ' TMS' != qmslayer['name']:
+            find_changes = find_changes + ';' + 'name'
+        if 'extent' in layer.keys():
+            if 'z_max' not in qmslayer.keys(): qmslayer['z_max'] = ''
+            if 'z_min' not in qmslayer.keys(): qmslayer['z_min'] = ''
+            if 'max_zoom' in layer['extent'].keys() and layer['extent']['max_zoom'] != qmslayer['z_max']:
+                find_changes = find_changes + ';' + 'zmax'
+            if 'min_zoom' in layer['extent'].keys() and layer['extent']['min_zoom'] != qmslayer['z_min']:
+                find_changes = find_changes + ';' + 'zmin'
+            
+    return find_changes        
 def url_osmlabTMS2qms(url):
     if 'switch' in url:
         regex = r'\{switch:.*?\}'
@@ -136,16 +226,13 @@ def getParamsWMS(url):
     
 if __name__ == '__main__':
 
-    if not os.path.exists('qms_full.json'): downloadqms()
-    qmslist=openqmsfile()
-    
-    #read file from github
-    url='https://github.com/osmlab/editor-layer-index/raw/gh-pages/imagery.json'
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    print('Fresh OSMLab imagery.json dowloaded')
+    if not os.path.exists('qms_full.json') or args.update: downloadQMS('qms.json')
+    if not os.path.exists('imagery.json') or args.update: downloadOSMLab('imagery.json')
 
-    fieldnames = ['id', 'name', 'type', 'exist_qms', 'url','url_qms','layers_qms','format_qms','getparams_qms','country_code','start_date','end_date','min_zoom','max_zoom','best','overlay','license_url','attribution_text','attribution_url','available_projections']
+    data = openjson('imagery.json')
+    qmslist=openjson('qms_full.json')
+    
+    fieldnames = ['id', 'name', 'type', 'exist_qms', 'changes_sync', 'url','url_qms','layers_qms','format_qms','getparams_qms','country_code','start_date','end_date','min_zoom','max_zoom','best','overlay','license_url','attribution_text','attribution_url','available_projections']
     
     with open('list.csv', 'wb') as csvfile:
         listwriter = csv.DictWriter(csvfile, fieldnames, delimiter=';',quotechar='"', quoting=csv.QUOTE_ALL)
@@ -170,9 +257,9 @@ if __name__ == '__main__':
             #Check if services already exists in QMS based on it's url
             exist_qms = ''
             if row['type'] == 'tms':
-                exist_qms = find_qmsTMS(row['url_qms'])
+                qmslayer,exist_qms = find_qmsTMS(row['url_qms'])
             elif row['type'] == 'wms':
-                exist_qms = find_qmsWMS(row['url_qms'],row['layers_qms'])
+                qmslayer,exist_qms = find_qmsWMS(row['url_qms'],row['layers_qms'])
             row['exist_qms'] = exist_qms
 
             row['id'] = layer.get('id')
@@ -198,5 +285,11 @@ if __name__ == '__main__':
                     row['max_zoom'] = layer['extent']['max_zoom']
                 if 'min_zoom' in layer['extent']:
                     row['min_zoom'] = layer['extent']['min_zoom']
+
+            if exist_qms:
+                changes_exist = find_changes(qmslayer,layer)
+                row['changes_sync'] = changes_exist
+            else:
+                if find_noimport(row['id']): row['exist_qms'] = 'Skip'
 
             listwriter.writerow(row)
