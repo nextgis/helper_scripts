f29dd7ac509bbbbabcd99019b76ec27c43b33588
Artem Svetlov <trolleway@yandex.ru>
2019-06-04 13:41:30 +0300

    Update config.example.py

--- photos2ngw/config.example.py
+++ photos2ngw/config.example.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 #ngw api
-ngw_url = 'http://demo.nextgis.com'
-ngw_creds = ('administrator', 'admin')
+ngw_url = 'http://dev.nextgis.com/sandbox'
+ngw_creds = ('administrator', 'demodemo')
