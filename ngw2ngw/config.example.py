dec0f19995dd37f76acab1d64624ed84569ce92a
trolleway <trolleway@yandex.ru>
2019-07-01 13:39:40 +0300

    sandbox url into default config

--- ngw2ngw/config.example.py
+++ ngw2ngw/config.example.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 #ngw api
-ngw1_url = ''
+ngw1_url = 'http://dev.nextgis.com/sandbox/'
 ngw1_resource_id = ''
-ngw1_creds = ('', '')
+ngw1_creds = ('administrator', 'demodemo')
 ngw1_dump_today_filename = 'dump_ngw1_now.json'
 ngw1_dump_yesterday_filename = 'dump_ngw1_prev.json'
 
 #ngw api
-ngw2_url = ''
+ngw2_url = 'http://dev.nextgis.com/sandbox/'
 ngw2_resource_id = ''
-ngw2_creds = ('', '')
+ngw2_creds = ('administrator', 'demodemo')
 ngw2_dump_today_filename = 'dump_ngw2_now.json'
 ngw2_dump_yesterday_filename = 'dump_ngw2_prev.json'
 
