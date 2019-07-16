cdf1a952d0d0585e474cf65b3dc0928b4cd496bd
trolleway <trolleway@yandex.ru>
2019-07-16 18:02:40 +0300

    update crontab

--- ngw_replication/crontab-init.sh
+++ ngw_replication/crontab-init.sh
@@ -3,6 +3,7 @@
 echo "create cron file..."
 
 echo "* * * * * python /helper_scripts/ngw_replication/ngw_replication.py -url1 "$(cat /run/secrets/2019tigercounter_primary_ngw_url)" -layer1 "$(cat /run/secrets/2019tigercounter_primary_ngw_layer) -login1 $(cat /run/secrets/2019tigercounter_primary_ngw_login) -pass1 $(cat /run/secrets/2019tigercounter_primary_ngw_password) -url2 "$(cat /run/secrets/2019tigercounter_secondary_ngw_url)" -layer2 "$(cat /run/secrets/2019tigercounter_secondary_ngw_layer)" -login2 "$(cat /run/secrets/2019tigercounter_secondary_ngw_login)" -pass2 "$(cat /run/secrets/2019tigercounter_secondary_ngw_password)"  > /etc/cron.d/replication-cron
+echo "* * * * * python /helper_scripts/ngw_replication/ngw_replication.py -url1 "$(cat /run/secrets/2019tigercounter_primary_ngw_url)" -layer1 "$(cat /run/secrets/2019tigercounter_primary_ngw_layer2) -login1 $(cat /run/secrets/2019tigercounter_primary_ngw_login) -pass1 $(cat /run/secrets/2019tigercounter_primary_ngw_password) -url2 "$(cat /run/secrets/2019tigercounter_secondary_ngw_url)" -layer2 "$(cat /run/secrets/2019tigercounter_secondary_ngw_layer2)" -login2 "$(cat /run/secrets/2019tigercounter_secondary_ngw_login)" -pass2 "$(cat /run/secrets/2019tigercounter_secondary_ngw_password)"  > /etc/cron.d/replication-cron
 echo "# An empty line is required at the end of this file for a valid cron file." >> /etc/cron.d/replication-cron
 
 # Give execution rights on the cron job
