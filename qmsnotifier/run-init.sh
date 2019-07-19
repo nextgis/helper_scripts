7e84004c4f74400b40b293ff924193f7f992adfb
Maxim Dubinin <maxim.dubinin@nextgis.com>
2019-07-19 15:00:08 +0300

    docker for qmsnotifier

---
+++ qmsnotifier/run-init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "$SETUP_CRON root /home/qmsnotifier/run.sh > /proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/qmsnotifier-cron
+chmod 0644 /etc/cron.d/qmsnotifier-cron
+
+echo "Starting cron...."
+
+cron -f
---
+++ qmsnotifier/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "start"
+/usr/local/bin/python /home/qmsnotifier/qmsnotifier3.py
+echo "finish"
+
+exit 0
