98830fae33fb1f8dc1f04bf1a9105abf37aae746
trolleway <trolleway@yandex.ru>
2019-09-17 12:34:07 +0300

    header

--- grid/grid.py
+++ grid/grid.py
@@ -5,17 +5,10 @@
 # grid.py
 # ---------------------------------------------------------
 # Upload images to a Web GIS
-# More: https://gitlab.com/nextgis/helper_scripts
+# More: https://github.com/nextgis/helper_scripts
 #
-# Usage:
-#      grid.py [-h] [-o] [-of ORIGINALS_FOLDER]
-#      where:
-#           -h              show this help message and exit
-#           -o              overwrite
-#           -of             relative path to folder with originals
-#           -t              type of data, license or gin
 # Example:
-#      python grid.py -s 100 src.geojson dst.gpkg 
+#      python  grid.py --step 100 --mode rect bounds.geojson grid.gpkg 
 #
 # Copyright (C) 2019-present Artem Svetlov (artem.svetlov@nextgis.com)
 #
@@ -37,13 +30,7 @@
 #******************************************************************************
 
 import os, sys
-import requests
-import json
-import urllib2
 import argparse
-from copy import deepcopy
-from contextlib import closing
-import datetime
 from math import ceil
 
 import shapely
@@ -58,16 +45,20 @@ from osgeo import ogr, osr
 def get_args():
     epilog = '''Sample: '''
     epilog +=  '''
-    python grid.py -s 100 src.geojson dst.gpkg  '''
-    p = argparse.ArgumentParser(description="Generate grid of points into source polygons", epilog=epilog)
+    
+    python grid.py -s 100 src.geojson dst.gpkg 
+    Grid calculate in meters, using WGS84 / UTM projection. Boundary layer can contains features for different place of globe.
+     '''
+    p = argparse.ArgumentParser(description="Generate grid of points or squares into source polygons", epilog=epilog)
 
     p.add_argument('--step',required=False, default = 1000,help='Steps in meters')
     p.add_argument('--mode',required=False,choices=['point', 'rect'], default = 'point',help='Grid mode.')
+    p.add_argument('--clip',required=False,choices=['all', 'intersection','touches'], default = 'intersection',help="Grid clipping mode. \n all: grid in extent of each boundary feature. \n touches: leave features intersects or overlaps with boundary \n intersection: intersect grid by boundary")
 
     p.add_argument('src', help='Vector polygonal or multipolygonal file with boundaries. Any format, any projection.')
     p.add_argument('dest', help='Result file. Extension should be .gpkg or .shp')
 
-    p.add_argument('--verbose', '-v', help='messages', action='store_true')
+    #p.add_argument('--verbose', '-v', help='messages', action='store_true')
     #p.add_argument('--config', help='patch to config.py file',required=False)
     return p.parse_args()
 
@@ -106,6 +97,9 @@ def process():
     #-------------------------------------
     #use argparse
 
+
+
+
     gridWidth = float(args.step)
     gridHeight = float(args.step)
 
@@ -113,7 +107,7 @@ def process():
 
     inputfn = args.src
     outputGridfn = args.dest
-
+    clip_operator = args.clip
 
     filename, file_extension = os.path.splitext(outputGridfn)
     if file_extension.upper() == '.SHP':
@@ -136,15 +130,14 @@ def process():
     wgs1984SpatialRef.ImportFromEPSG(4326)
     coordTrans = osr.CoordinateTransformation(srcSpatialRef, wgs1984SpatialRef)
 
+    print outputGridfn
     outDriver = ogr.GetDriverByName(outputDriverName)
     if os.path.exists(outputGridfn):
         os.remove(outputGridfn)
     outDataSource = outDriver.CreateDataSource(outputGridfn)
     if mode == 'rect':
-        outLayer = outDataSource.CreateLayer(outputGridfn,wgs1984SpatialRef,geom_type=ogr.wkbPolygon)
+        outLayer = outDataSource.CreateLayer(outputGridfn,wgs1984SpatialRef,geom_type=ogr.wkbMultiPolygon)
     elif mode == 'point':
-        outLayer = outDataSource.CreateLayer(outputGridfn,wgs1984SpatialRef,geom_type=ogr.wkbPoint)
-    elif mode == 'point2':
         outLayer = outDataSource.CreateLayer(outputGridfn,wgs1984SpatialRef,geom_type=ogr.wkbPoint)
     featureDefn = outLayer.GetLayerDefn()
 
@@ -213,51 +206,11 @@ def process():
 
             while countrows < rows:
                 countrows += 1
-                if mode == 'rect_ogr':
-                    ring = ogr.Geometry(ogr.wkbLinearRing)
-                    ring.AddPoint(ringXleftOrigin, ringYtop)
-                    ring.AddPoint(ringXrightOrigin, ringYtop)
-                    ring.AddPoint(ringXrightOrigin, ringYbottom)
-                    ring.AddPoint(ringXleftOrigin, ringYbottom)
-                    ring.AddPoint(ringXleftOrigin, ringYtop)
-                    poly = ogr.Geometry(ogr.wkbPolygon)
-                    poly.AddGeometry(ring)
-                    poly.Transform(fromUTMcoordTrans)
 
-                    # add new geom to layer
-                    outFeature = ogr.Feature(featureDefn)
-                    outFeature.SetGeometry(poly)
-                    outLayer.CreateFeature(outFeature)
-                    outFeature = None
-                elif mode == 'point_ogr':
-                    ring = ogr.Geometry(ogr.wkbLinearRing)
-                    ring.AddPoint(ringXleftOrigin, ringYtop)
-                    ring.AddPoint(ringXrightOrigin, ringYtop)
-                    ring.AddPoint(ringXrightOrigin, ringYbottom)
-                    ring.AddPoint(ringXleftOrigin, ringYbottom)
-                    ring.AddPoint(ringXleftOrigin, ringYtop)
-                    poly = ogr.Geometry(ogr.wkbPolygon)
-                    poly.AddGeometry(ring)
-                    poly.Transform(fromUTMcoordTrans)
-                    grid_object = ogr.Geometry(ogr.wkbPoint)
-                    #print ring.ExportToWkt()
-                    centroid = poly.Centroid()
-                    grid_object.AddGeometry(centroid)
-
-                    #print centroid.ExportToWkt()
-
-                     # add new geom to layer
-                    outFeature = ogr.Feature(featureDefn)
-                    outFeature.SetGeometry(centroid)
-                    outLayer.CreateFeature(outFeature)
-                    outFeature = None
-
-                elif mode == 'rect':
+                if mode == 'rect':
                     shapely_grid_object_uncut = shapely.geometry.box(ringXleftOrigin, ringYbottom, ringXrightOrigin, ringYtop)
 
                 elif mode == 'point':
-
-                    #shapely_grid_object_uncut = shapely.geometry.box(ringXleftOrigin, ringYbottom, ringXrightOrigin, ringYtop).centroid
                     shapely_grid_object_uncut = shapely.geometry.box(ringXleftOrigin, ringYbottom, ringXrightOrigin, ringYtop)
 
                 #shapely_grid_object_uncut is UTM
@@ -266,7 +219,7 @@ def process():
 
                 #print shapely_boundary_utm.wkt
 
-                clip_operator = 'touches' #coveredby, intersects, all
+                #clip_operator = 'touches' #all, touches, intersection
 
                 shapely_grid_object_cutted = None
                 if clip_operator == 'all':
