69a6adaf2ebce6474681b5f6a2de3a3cda807445
Maxim Dubinin <maxim.dubinin@nextgis.com>
2019-09-20 00:12:35 +0300

    ngw permissions checking

---
+++ ngw_check_permissions/ngw_check_permissions.py
@@ -0,0 +1,23 @@
+#Outputs a list of ids of resources for which permissions are set
+#ngw_check_permissions.py -u demo -r https
+
+import requests
+import argparse
+from requests.auth import HTTPBasicAuth
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r','--protocol',default='http',help='http or https')
+parser.add_argument('-u','--url', required=True, help='Web GIS name')
+parser.add_argument('-n','--name', help='Username')
+parser.add_argument('-p','--password', help='Password')
+args = parser.parse_args()
+
+
+url = '%s://%s.nextgis.com/api/resource/search/' % (args.protocol,args.url)
+print(url)
+
+items = requests.get(url, auth=HTTPBasicAuth(args.name,args.password)).json()
+
+for item in items:
+    if item['resource']['permissions'] != []:
+        print(item['resource']['id'])
--- qmsnotifier/run-init.sh
+++ qmsnotifier/run-init.sh
@@ -3,6 +3,6 @@
 echo "$SETUP_CRON root /home/qmsnotifier/run.sh > /proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/qmsnotifier-cron
 chmod 0644 /etc/cron.d/qmsnotifier-cron
 
-echo "Starting cron...."
+echo "Starting cron....Waiting for execution..."
 
 cron -f
