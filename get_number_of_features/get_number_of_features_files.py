c2a8dee38c50425e9c2300cc3784f7d500d0103e
Maxim Dubinin <maxim.dubinin@nextgis.com>
2023-01-03 17:28:41 +0300

    gixes

--- get_number_of_features/get_number_of_features_files.py
+++ get_number_of_features/get_number_of_features_files.py
@@ -2,7 +2,6 @@
 #get number of features for latest 7z for each subfolder in a folder
 #run 
 #sudo -u postgres psql -d osmshp -t -A -F";" -c "SELECT id, code FROM region" > regs.csv
-#on pluk before running this script
 #then move results.csv to data.nextgis.com
 
 import os
@@ -13,68 +12,77 @@ import platform
 from osgeo import ogr
 
 if platform.uname()[0] == 'Windows':
-    path = 'c:/tools/7-Zip/'
+    zippath = 'c:/tools/'
 else:
-    path = ''
+    zippath = ''
 
 cur_dir = os.getcwd()
-data_dir = '//nextgis-nas/share/data/cntry/'
+data_dir = 'd:/Programming/Python/helper_scripts/get_number_of_features/data/' #'d:\\Programming\\Python\\unpack-convert-repack\\data\\' #'//han.nextgis.net/share/data/pbf/shp/' #'c:\\Work\\' \\han.nextgis.net\share\data\pbf\shp\
 #data_dir = '//nextgis-nas/share/data/'
 dst_dir = 'd:/Programming/Python/helper_scripts/get_number_of_features/'
 
-
 os.chdir(data_dir)
 files = glob.glob('*.zip')
+files.sort()
 os.chdir(dst_dir)
 
 output = open('result.csv','w')
-output.write('REG;LAYER;NUM\n')
+#output.write('REG;LAYER;NUM\n')
 
 for f in files:
-    if 'cntry' in data_dir:
-        f_reg = f.split('-')[0]
-    else:
-        f_reg = f.split('-')[0] + '-' + f.split('-')[1]
+    f_reg = f.replace('-gpkg.zip','')
     print('Processing ' + f)
-        
-    #unpack
-    shutil.copy(data_dir + f,dst_dir)
-    os.chdir(cur_dir)
-    cmd = path + '7z x -otemp ' + f + ' >nul'
-    os.system(cmd)
-    #if os.path.exists('nul'): os.remove('nul')
-    os.chdir('temp/data')
     
-    #calculate
-    list_of_layers = glob.glob('*.shp')
-    for layer_shp in list_of_layers:
-        if 'boundary-polygon-' not in layer_shp:
-            layer_name = layer_shp.replace('.shp','')
-            driver = ogr.GetDriverByName("ESRI Shapefile")
+    if os.path.getsize(data_dir + f) > 22:
+
+        #unpack
+        shutil.copy(data_dir + f,dst_dir)
+        os.chdir(cur_dir)
+        cmd = zippath + 'unzip -q ' + f + ' -d temp'
+        os.system(cmd)
+        #if os.path.exists('nul'): os.remove('nul')
+        os.chdir('temp')
+        
+        #calculate
+        list_of_layers = glob.glob('data\*.gpkg')
+        for layer_shp in list_of_layers:
+            if '-lvl' not in layer_shp:
+                layer_name = layer_shp.replace('.gpkg','')
+                driver = ogr.GetDriverByName("GPKG")
+                source_ds = driver.Open(layer_shp, 0)
+                layer = source_ds.GetLayerByIndex(0)
+                res = layer.GetFeatureCount()
+                
+                output.write(f_reg + ';' + layer_name + ';' + str(res) + '\n')
+                source_ds.Destroy()
+
+        list_of_layers = glob.glob('*.csv')
+        for layer_shp in list_of_layers:
+            layer_name = layer_shp.replace('.csv','')
+            driver = ogr.GetDriverByName("CSV")
             source_ds = driver.Open(layer_shp, 0)
             layer = source_ds.GetLayerByIndex(0)
             res = layer.GetFeatureCount()
             
             output.write(f_reg + ';' + layer_name + ';' + str(res) + '\n')
-
             source_ds.Destroy()
 
-    list_of_layers = glob.glob('*.gpkg')
-    for layer_gpkg in list_of_layers:
-        if 'boundary-polygon-' not in layer_gpkg:
-            layer_name = layer_gpkg.replace('.gpkg','')
-            driver = ogr.GetDriverByName("GPKG")
-            source_ds = driver.Open(layer_gpkg, 0)
-            layer = source_ds.GetLayerByIndex(0)
-            res = layer.GetFeatureCount()
-            
-            output.write(f_reg + ';' + layer_name + ';' + str(res) + '\n')
+        list_of_layers = glob.glob('*.gpkg')
+        for layer_gpkg in list_of_layers:
+            if 'boundary-polygon-' not in layer_gpkg:
+                layer_name = layer_gpkg.replace('.gpkg','')
+                driver = ogr.GetDriverByName("GPKG")
+                source_ds = driver.Open(layer_gpkg, 0)
+                layer = source_ds.GetLayerByIndex(0)
+                res = layer.GetFeatureCount()
+                
+                output.write(f_reg + ';' + layer_name + ';' + str(res) + '\n')
 
-            source_ds.Destroy()
+                source_ds.Destroy()
 
-    #cleanup
-    os.chdir('../../')
-    shutil.rmtree('temp')
-    os.remove(f)
+        #cleanup
+        os.chdir('..')
+        shutil.rmtree('temp')
+        os.remove(f)
 
 output.close()
