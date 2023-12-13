0e9e0b6147e845a0e83f40e35111b7950e774da7
Maxim Dubinin <maxim.dubinin@nextgis.com>
2023-12-13 15:40:36 +0300

    fix ugly paths

--- get_number_of_features/get_number_of_features_files.py
+++ get_number_of_features/get_number_of_features_files.py
@@ -17,8 +17,8 @@ else:
     zippath = ''
 
 cur_dir = os.getcwd()
-dst_dir = 'd:/Programming/Python/helper_scripts/get_number_of_features/'
-data_dir = dst_dir + '/data/' 
+dst_dir = os.getcwd()
+data_dir = os.path.join(dst_dir,'data')
 
 os.chdir(data_dir)
 files = glob.glob('*.zip')
@@ -33,7 +33,6 @@ format = 'gpkg'
 #format = 'shape'
 
 output = open('result.csv','w')
-#output.write('REG;LAYER;NUM\n')
 
 for f in files:
     f_reg = f.replace('-%s.zip' % format,'')
@@ -56,7 +55,7 @@ for f in files:
             
         for layer in list_of_layers:
             if '-lvl' not in layer:
-                layer_name = layer.replace('.' + ext,'')
+                layer_name = layer.replace('.' + ext,'').replace('data\\','')
                 driver = ogr.GetDriverByName(driver_str)
                 source_ds = driver.Open(layer, 0)
                 layer = source_ds.GetLayerByIndex(0)
