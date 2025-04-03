41f8b26e3cb2b0e54b58d40a1a12e9b096661eec
Artem Svetlov <trolleway@yandex.ru>
2025-04-03 13:40:07 +0300

    ngw_replication: update syntax to python3

--- ngw_replication/ngw_replication.py
+++ ngw_replication/ngw_replication.py
@@ -58,7 +58,6 @@
 import os, sys
 import requests
 import json
-import urllib2
 import argparse
 from copy import deepcopy
 from contextlib import closing
@@ -119,7 +118,7 @@ try:
 
 except:
     path = os.path.abspath(args.config)
-    print "config.py not found at {path} Copy config.example.py to config.py, and set your Web GIS credentials here. See readme.md".format(path=path)
+    print("config.py not found at {path} Copy config.example.py to config.py, and set your Web GIS credentials here. See readme.md".format(path=path))
     quit()
 '''
 
@@ -144,12 +143,12 @@ class Replicator():
         try:
             url = ngw_url + '/api/resource/' +  str(layer_id) + '/feature/'
             if self.debug:
-                print url
+                print(url)
             req = requests.get(url,  auth=ngw_creds)
             req.raise_for_status()
 
         except requests.exceptions.RequestException as e:  # This is the correct syntax
-            print e
+            print(e)
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
@@ -187,8 +186,8 @@ class Replicator():
             block = dump[i:i+step]
             url = ngw_url + '/api/resource/' + str(layer_id) + '/feature/'
             if self.debug:
-                print 'perform patch query'
-                print url
+                print('perform patch query')
+                print(url)
             try:
                 #more smart requests call for easer debug
                 req = requests.Request('PATCH',url ,data=json.dumps(block), auth=ngw_creds)
@@ -203,7 +202,7 @@ class Replicator():
 
             #error handling for query
             except requests.exceptions.RequestException as e:  # This is the correct syntax
-                print e
+                print(e)
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
                 print(exc_type, fname, exc_tb.tb_lineno)
@@ -212,14 +211,13 @@ class Replicator():
 
             i = i + step
         if self.debug:
-            print 'vector features creation completed'
+            print('vector features creation completed')
 
     def get_match_feature_id_by_order(self,object_number,dump):
         match_object = dump[object_number]
         return match_object['id']
 
     def feature_has_atachments(self,feature):
-        print feature
         if feature['extensions']['attachment'] is None:
             return False
         else:
@@ -253,7 +251,7 @@ class Replicator():
         #assume that both dumps have same features count (already checked before)
         #return true when attachments have same size and filename
 
-        if self.debug: print 'check is_atachments_equal'
+        if self.debug: print('check is_atachments_equal')
         i = 0
         cnt = len(primary_layer_dump)
         for feature in primary_layer_dump:
@@ -268,7 +266,7 @@ class Replicator():
             if (primary_attachments is not None and secondary_attachments is None): #one layer have attachments, other not have
                 return False
             if len(primary_attachments) != len(secondary_attachments):
-                if self.debug: print 'feature {id} unmatch attachments count'.format(id=primary_layer_dump[i]['id'])
+                if self.debug: print('feature {id} unmatch attachments count'.format(id=primary_layer_dump[i]['id']))
                 return False
 
             #if attachment has deleted and changed to other file
@@ -289,12 +287,12 @@ class Replicator():
         try:
             url = primary_ngw_url + '/api/resource/' +  str(primary_layer_id) + '/feature_count'
             if self.debug:
-                print url
+                print(url)
             req = requests.get(url,  auth=primary_ngw_creds)
             req.raise_for_status()
 
         except requests.exceptions.RequestException as e:  # This is the correct syntax
-            print e
+            print(e)
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
@@ -306,12 +304,12 @@ class Replicator():
         try:
             url = secondary_ngw_url + '/api/resource/' +  str(secondary_layer_id) + '/feature_count'
             if self.debug:
-                print url
+                print(url)
             req = requests.get(url,  auth=secondary_ngw_creds)
             req.raise_for_status()
 
         except requests.exceptions.RequestException as e:  # This is the correct syntax
-            print e
+            print(e)
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
@@ -359,20 +357,20 @@ class Replicator():
         while i < features_count:
             feature_primary = dump[i]
             if self.debug:
-                print i
+                print(i)
             if self.feature_has_atachments(feature_primary):
-                if self.debug: print 'feature has attachments'
+                if self.debug: print('feature has attachments')
 
                 #secondary_layer_feature_id = self.get_match_feature_id_by_order(object_number=i,dump = dump_secondary)
                 #secondary_feature_id = dump_secondary[i]['id']
                 #now feature in secondary layer do not have attachments
                 #копирование атачментов из фичи одного слоя в фичу другого
                 for attachment in feature_primary['extensions']['attachment']:
-                    if self.debug: print  'upload attachment ' + attachment['name']
+                    if self.debug: print('upload attachment ' + attachment['name'])
                     #here will be creds of primary layer
                     #todo скачивание аттачментов с паролем
                     attachment_url=primary_ngw_url + '/api/resource/' + primary_layer_id + '/feature/' + str(feature_primary['id']) + '/attachment/' + str(attachment['id']) + '/download'
-                    if self.debug: print 'get attachment url: ', attachment_url
+                    if self.debug: print('get attachment url: ', attachment_url)
                     #Get file from internet, optionally with auth
                     with closing(requests.get(attachment_url, auth=primary_ngw_creds, stream=True)) as f:
 
@@ -381,7 +379,6 @@ class Replicator():
                         url = secondary_ngw_url + '/api/component/file_upload/upload'
                         req = requests.put(url, data=f.content, auth=secondary_ngw_creds)
 
-                        #print req.content
                         json_data = req.json()
                         json_data['name'] = attachment['name']
 
@@ -407,19 +404,19 @@ class Replicator():
                     #here will be creds of secondary layer
 
                     url = secondary_ngw_url + '/api/component/file_upload/upload'
-                    if self.debug: print  'Upload attachment to ' + url
+                    if self.debug: print('Upload attachment to ' + url)
                     req = requests.post(url,  data=payload, auth=secondary_ngw_creds, headers=headers)
 
-                    print req
+                    print(req)
 
                     json_data = req.json()
                     attach_data = {}
                     attach_data['file_upload'] = json_data
 
-                    if self.debug: print  'Грузим атачмент'
+                    if self.debug: print('Грузим атачмент')
                     secondary_feature_id = dump_secondary[i]['id']
                     url = secondary_ngw_url + '/api/' + '/resource/' + secondary_layer_id + '/feature/'+  str(secondary_feature_id) + '/attachment/'
-                    if self.debug: print url
+                    if self.debug: print(url)
                     req = requests.post(url,  data=json.dumps(attach_data), auth=secondary_ngw_creds, headers=headers)
                     '''
             i=i+1
@@ -445,7 +442,6 @@ if feature_count_equal:
     all_features_equal = replicator.is_layers_features_equal(primary_ngw_url=config.primary_ngw_url, primary_ngw_layer_id = config.primary_ngw_layer_id, primary_ngw_creds = config.primary_ngw_creds,
     secondary_ngw_url=config.secondary_ngw_url, secondary_ngw_layer_id = config.secondary_ngw_layer_id, secondary_ngw_creds = config.secondary_ngw_creds)
 
-#print update time in metadata, for control
 result = replicator.add_metadata(ngw_url=config.secondary_ngw_url, layer_id = config.secondary_ngw_layer_id, ngw_creds = config.secondary_ngw_creds,key='CHECKED_AT',value=str(datetime.datetime.now()))
 if all_features_equal:
     quit('all features equal')
@@ -480,5 +476,4 @@ primary_layer_dump = replicator.get_ngw_layer_dump(ngw_url=config.primary_ngw_ur
 result = replicator.replicate_attachments(primary_ngw_url=config.primary_ngw_url, primary_layer_id = config.primary_ngw_layer_id, primary_ngw_creds = config.primary_ngw_creds,
 secondary_ngw_url=config.secondary_ngw_url, secondary_layer_id = config.secondary_ngw_layer_id, secondary_ngw_creds = config.secondary_ngw_creds, dump = primary_layer_dump)
 
-#print update time in metadata, for control
 result = replicator.add_metadata(ngw_url=config.secondary_ngw_url, layer_id = config.secondary_ngw_layer_id, ngw_creds = config.secondary_ngw_creds,key='UPDATED_AT',value=str(datetime.datetime.now()))
